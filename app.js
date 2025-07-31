3f2ff99dbc3084bc5d6aa62803170272238e5158
Marcelo Rodrigues <marcelo--rodrigues2@hotmail.com>
2025-07-31 17:08:25 -0300

    feat: função para adicionar amigos a lista

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigosArr = [];
+let amigo = document.getElementById("amigo");
+
+function addAmigoHandler(amigo) {
+  if (amigo.value == "" || amigosArr.includes(amigo.value)) {
+    alert("Digite um nome válido para o seu amigo!");
+    return;
+  }
+
+  return amigo;
+}
+
+function adicionarAmigo() {
+  const amigoValidated = addAmigoHandler(amigo);
+  if (amigoValidated) {
+    amigosArr.push(amigoValidated.value);
+    console.log(amigosArr);
+  }
+
+  return;
+}
+
+console.log(amigosArr);
